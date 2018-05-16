9cb0dc0b820373356b7dd04a7f82b0e213fa640c
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-17 02:36:46 +0900

    Update

--- NN.jl
+++ NN.jl
@@ -7,9 +7,21 @@ const number_of_middle_layer_node = 4
 sigmoid(s) = 1 / (1 + e^-s)
 update(x, y, t, w) = w - 2ε * x * y * (y - t) * (1 - y)
 
-# 各層のノードの数の配列
+# 各層のノード数の配列
 node = vcat([number_of_input_node], fill(number_of_middle_layer_node, number_of_layers - 2), [number_of_output_node])
 
 # 重みのタプル
 ws = ntuple(i -> rand(node[i], node[i + 1]), length(node) - 1)
 
+us = let
+    local prev
+    ntuple(
+        i ->
+            if i == 1
+                prev = eye(number_of_input_node)
+            else
+                prev = ws[i - 1]' * prev
+            end,
+        number_of_layers
+    )
+end
