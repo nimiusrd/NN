cf3f8f8c8e96ab2720f0305ea261ec45c8c4ee2e
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-18 15:52:02 +0900

    Update

--- NN.jl
+++ NN.jl
@@ -9,22 +9,22 @@ update_out_layer(x, y, w, t) = w - 2ε * x * y * (1 - y) * (y - t)
 update_mid_layer(x, y, w, Y) = w - ε * x * Y * (1 - Y) * y 
 
 # データの読み込み
-train = open("train", "r") do io
+x_train = open("train", "r") do io
     [
-        [
+        vec([
             parse(Float64, str)
             for str in split(l, " ")
-        ]
+        ])
         for l in readlines(io)
     ]
 end
 
-test = open("test", "r") do io
+y_train = open("test", "r") do io
     [
-        [
+        vec([
             parse(Float64, str)
             for str in split(l, " ")
-        ]
+        ])
         for l in readlines(io)
     ]
 end
@@ -35,33 +35,45 @@ node = vcat([number_of_input_node], fill(number_of_middle_layer_node, number_of_
 # 重みのタプル
 ws = ntuple(i -> rand(node[i], node[i + 1]), length(node) - 1)
 
-ys = let
-    local prev
-    ntuple(
-        i ->
-            if i === 1
-                prev = eye(number_of_input_node)
-                [
-                    sigmoid(sum(prev[i, :]))
-                    for i=1:size(prev)[1]
-                ]
-            else
-                prev = ws[i - 1]' * prev
-                [
-                    sigmoid(sum(prev[i, :]))
-                    for i=1:size(prev)[1]
-                ]
-            end,
-        number_of_layers
+function train(ws, input, test)
+    ys = let
+        local prev
+        ntuple(
+            i ->
+                if i === 1
+                    prev = eye(number_of_input_node) .* input
+                    [
+                        sigmoid(sum(prev[i, :]))
+                        for i=1:size(prev)[1]
+                    ]
+                else
+                    prev = ws[i - 1]' * prev
+                    [
+                        sigmoid(sum(prev[i, :]))
+                        for i=1:size(prev)[1]
+                    ]
+                end,
+            number_of_layers
+        )
+    end
+
+    ws = ntuple(i ->
+        reshape([
+            i === length(node) - 1 ? update_out_layer(ys[i][x], ys[i + 1][y], ws[i][x, y], test[Y]) : update_mid_layer(ys[i][x], ys[i + 1][y], ws[i][x, y], ys[length(node)][Y])
+            for x=1:node[i]
+            for y=1:node[i + 1]
+            for Y=1:node[length(node)]
+        ], (node[i], node[i + 1])),
+        length(node) - 1
     )
+
+    ws
 end
 
-ws = ntuple(i ->
-    reshape([
-        i === length(node) - 1 ? update_out_layer(ys[i][x], ys[i + 1][y], ws[i][x, y], 1) : update_mid_layer(ys[i][x], ys[i + 1][y], ws[i][x, y], ys[length(node)][Y])
-        for x=1:node[i]
-        for y=1:node[i + 1]
-        for Y=1:node[length(node)]
-    ], (node[i], node[i + 1])),
-    length(node) - 1
-)
+let
+    new_ws = ws
+    for i=1:length(x_train)
+        new_ws = train(new_ws, x_train[i], y_train[i])
+        println(new_ws)
+    end
+end
