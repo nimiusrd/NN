6767baf61f67782339252bda9582efd9748d4303
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-22 16:08:05 +0900

    Update

--- NN.jl
+++ NN.jl
@@ -5,71 +5,36 @@ const number_of_output_node = 1
 const number_of_layers = 3
 const number_of_middle_layer_node = 4
 const learning_limit = 500
+const node = [[number_of_input_node]; fill(number_of_middle_layer_node, number_of_layers - 2); [number_of_output_node]]
 
 sigmoid(s) = 1 / (1 + e^-s)
 calc_out_layer_delta(y, t) = ε * (1 - y) * y * 2 * (y - t) 
-function calc_mid_layer_delta(y, ws, delta)
-    @show ws
-    @show delta
-    ε * (1 - y) * y * dot(ws, delta)
-end
+calc_mid_layer_delta(y, ws, delta) = ε * (1 - y) * y * dot(ws, delta)
 loss(y, t) = (y - t).^2
 function get_ys(ws, input)
     local prev
     ntuple(
         i ->
             if i === 1
-                prev = eye(number_of_input_node) .* input
-                [input; -θ]
+                prev = map(
+                    sigmoid,
+                    input
+                )
             elseif i === number_of_layers
-                prev = ws[i - 1]' * prev
-                [
-                    sigmoid(sum(prev[i, :]))
-                    for i=1:size(prev)[1]
-                ]
+                prev = map(
+                    sigmoid,
+                    [ws[i - 1]; fill(-θ, (1, node[i]))]' * [prev; 1]
+                )
             else
-                prev = ws[i - 1]' * prev
-                [
-                    [
-                        sigmoid(sum(prev[i, :]))
-                        for i=1:size(prev)[1]
-                    ];
-                    [-θ]
-                ]
+                prev = map(
+                    sigmoid,
+                    [ws[i - 1]; fill(-θ, (1, node[i]))]' * [prev; 1]
+                )
             end,
         number_of_layers
     )
 end
 
-# データの読み込み
-x_train = open("train", "r") do io
-    [
-        vec([
-            parse(Float64, str)
-            for str in split(l, " ")
-        ])
-        for l in readlines(io)
-    ]
-end
-
-y_train = open("test", "r") do io
-    [
-        vec([
-            parse(Float64, str)
-            for str in split(l, " ")
-        ])
-        for l in readlines(io)
-    ]
-end
-
-# 各層のノード数の配列
-node = [[number_of_input_node]; fill(number_of_middle_layer_node, number_of_layers - 2); [number_of_output_node]]
-
-# 重みの初期化
-ws = ntuple(
-    i -> rand(node[i], node[i + 1]),
-    length(node) - 1
-)
 function train(ws, input, test)
     ys = get_ys(ws, input)
     ws = let
@@ -85,7 +50,7 @@ function train(ws, input, test)
                     reshape(
                         [
                             ws[i - 1][x, y] - delta[y] * ys[i - 1][x]
-                            for x = 1:node[i - 1] 
+                            for x = 1:node[i - 1]
                             for y = 1:node[i]
                         ],
                         (node[i - 1], node[i])
@@ -122,6 +87,33 @@ function test(ws, input)
     ys = get_ys(ws, input)
     ys[number_of_layers]
 end
+
+# データの読み込み
+x_train = open("train", "r") do io
+    [
+        vec([
+            parse(Float64, str)
+            for str in split(l, " ")
+        ])
+        for l in readlines(io)
+    ]
+end
+
+y_train = open("test", "r") do io
+    [
+        vec([
+            parse(Float64, str)
+            for str in split(l, " ")
+        ])
+        for l in readlines(io)
+    ]
+end
+
+# 重みの初期化
+ws = ntuple(
+    i -> rand(node[i], node[i + 1]),
+    length(node) - 1
+)
 
 let
     c = 0
