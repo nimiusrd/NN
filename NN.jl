ce1b06d6e78b5f772840aa122a765a918228e36e
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-26 14:17:55 +0900

    Update

--- NN.jl
+++ NN.jl
@@ -2,14 +2,16 @@ const NUMBER_OF_INPUT_NODE = 3 # 入力層のノード数
 const NUMBER_OF_OUTPUT_NODE = 1 # 出力層のノード数
 const NUMBER_OF_MIDDLE_LAYER_NODE = 4 # 中間層のノード数
 const NUMBER_OF_LAYERS = 3 # 層数
-const ε = 0.75 # 学習率
-const LEARNING_LIMIT = 10000 # 最大学習数
-const ERROR_LIMIT = 0.001 # 許容する誤差の上限
+const ε = 0.01 # 学習率
+const LEARNING_LIMIT = 100000 # 最大学習数
+const ERROR_LIMIT = 0.01 # 許容する誤差の上限
 
 const node = [[NUMBER_OF_INPUT_NODE + 1]; fill(NUMBER_OF_MIDDLE_LAYER_NODE + 1, NUMBER_OF_LAYERS - 2); [NUMBER_OF_OUTPUT_NODE]]
-sigmoid = θ -> s -> 1 / (1 + e^-(s - θ))
-calc_out_layer_delta(y, t) = ε * (1 - y) * y * 2 * (y - t) 
-calc_mid_layer_delta(y, ws, delta) = ε * (1 - y) * y * dot(ws, delta)
+sigmoid = s -> 1 / (1 + e^-s)
+calc_out_layer_delta(y, t) = (1 - y) * y * 2 * (y - t) 
+function calc_mid_layer_delta(y, w, delta)
+    (1 - y) * y * w * delta
+end
 loss(y, t) = (y - t).^2
 function read_data(filename)
     open(filename, "r") do io
@@ -30,17 +32,9 @@ function get_ys(ws, input)
                 prev = input
                 [prev; 1]
             elseif i === NUMBER_OF_LAYERS
-                prev = ws[i - 1]' * [prev; 1]
-                prev = [
-                    sigmoid(ws[i - 1][node[i - 1], s])(prev[s])
-                    for s = 1:node[i]
-                ]
+                prev = map(sigmoid, ws[i - 1]' * [prev; 1])
             else
-                prev = ws[i - 1]' * [prev; 1]
-                prev = [
-                    sigmoid(ws[i - 1][node[i - 1], s])(prev[s])
-                    for s = 1:node[i] - 1
-                ]
+                prev = map(sigmoid, ws[i - 1]' * [prev; 1])
                 [prev; 1]
             end,
         NUMBER_OF_LAYERS
@@ -60,30 +54,33 @@ function train(ws, input, test)
                 new_ws = (
                     reshape(
                         [
-                            ws[i - 1][x, y] - delta[y] * ys[i - 1][x]
+                            ws[i - 1][x, y] - ε * delta[y] * ys[i - 1][x]
                             for x = 1:node[i - 1]
                             for y = 1:node[i]
                         ],
-                        (node[i - 1], node[i])
-                    ),
+                        (node[i], node[i - 1])
+                    )',
                     new_ws...
                 )
-                prev_delta = delta
+                prev_delta = delta .* ws[i - 1][1:length(ws[i - 1]) - 1]
             else
-                delta = [
-                    calc_mid_layer_delta(ys[i][y], ws[i][x, :], prev_delta)
-                    for x = 1:node[i]
-                    for y = 1:node[i + 1]
-                ]
+                delta = reshape(
+                    [
+                        calc_mid_layer_delta(ys[i][y], ws[i - 1][x, y], prev_delta[y])
+                        for x = 1:node[i - 1]
+                        for y = 1:node[i] - 1
+                    ],
+                    (node[i] - 1, node[i - 1])
+                )'
                 new_ws = (
                     reshape(
                         [
-                            ws[i - 1][x, y] - delta[y] * ys[i - 1][x]
+                            ws[i - 1][x, y] - ε * delta[x, y] * ys[i - 1][x]
                             for x = 1:node[i - 1] 
                             for y = 1:node[i] - 1
                         ],
-                        (node[i - 1], node[i] - 1)
-                    ),
+                        (node[i] - 1, node[i - 1])
+                    )',
                     new_ws...
                 )
                 prev_delta = delta
@@ -91,7 +88,7 @@ function train(ws, input, test)
         end
         new_ws
     end
-    ws, ys[NUMBER_OF_LAYERS]
+    ws
 end
 function test(ws, input)
     ys = get_ys(ws, input)
@@ -119,12 +116,12 @@ let
     err = ERROR_LIMIT * length(x_train) + 1
     while sum(err) / length(x_train) > ERROR_LIMIT && LEARNING_LIMIT > c
         err = zeros(NUMBER_OF_OUTPUT_NODE)
-        ws = let
-            for i=1:length(x_train)
-                ws, y = train(ws, x_train[i], y_train[i])
-                err += loss(y, y_train[i])
-            end
-            ws
+        for i=1:length(x_train)
+            ws = train(ws, x_train[i], y_train[i])
+        end
+        for i=1:length(x_train)
+            ys = get_ys(ws, x_train[i])
+            err += loss(ys[NUMBER_OF_LAYERS], y_train[i])
         end
         c += 1
         @show c
